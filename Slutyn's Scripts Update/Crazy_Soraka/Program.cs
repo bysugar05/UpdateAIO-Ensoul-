fb4b0dd8bcb7dc492a114feb99f80a9067c5cdfc
Death <52470824+DeathGodx@users.noreply.github.com>
2019-07-26 22:05:26 +0300

    Add files via upload

---
+++ Slutyn's Scripts Update/Crazy_Soraka/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LoadEncrypt
+{
+    using System;
+    using System.Reflection;
+    using System.Security.Permissions;
+
+    using EnsoulSharp.SDK;
+
+    using LoadEncrypt.Properties;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+
+        [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
+        private static void OnGameLoad()
+        {
+            try
+            {
+                var a = Assembly.Load(Resources.Script);
+                var myType = a.GetType("Script.Example");// namespace + class name
+                var methon = myType.GetMethod("Init", BindingFlags.Public | BindingFlags.Static); // methon
+
+                if (methon != null)
+                {
+                    methon.Invoke(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
